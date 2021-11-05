e3763e466becb230249b2bec8e39717cd92da912
Rohan Arora <arorarohan4@gmail.com>
2021-11-05 18:01:09 +0530

    Completed Stack Implementation Using Vector.

--- Stack/Stack/StackUsingVector.hpp
+++ Stack/Stack/StackUsingVector.hpp
@@ -10,6 +10,7 @@
 
 #include <stdio.h>
 #include <vector>
+#include <exception>
 namespace mylib {
 
 /*Stack Works on the Principle of LIFO . Last in First Out.*/
@@ -21,22 +22,103 @@ private:
     int dataCount{-1};
     
 public:
+    /*Creates a Stack of specifies Size.*/
+    StackUsingVector(size_t sizeOfStack);
+    
     /*Checks Whether the Stack is Empty. Returns True if the Stack is Empty or else returns False*/
     bool isEmpty() const;
     
     /*Checks Whether the Stack is Full. Returns True if the Stack is Full or else return False. */
     bool isFull() const;
     
-    /*Returns the Copy of topmost Element of the Stack */
+    /*Returns the Copy of topmost Element of the Stack .
+     Throws UnderFlow Exception if we try to See/Peak to Top Most element of the Stack if it is Empty.*/
     T top() const;
     
-    /*Returns the Copy of Top Most Element of Stack and Removes the Top Most Element */
+    /*Returns the Copy of Top Most Element of Stack and Removes the Top Most Element .
+     Throws UnderFlow Exception if we try to pop out elements if the Stack is Empty.*/
     T pop() ;
     
     /*Pushes the data into the Stack.Returns True if the data is inserted successfully into the Stack or else returns False. */
-    bool push(T &data);
+    bool push(T data);
+    
+    
+    /*Prints All Elements from the Stack in LIFO order.*/
+    void PrintStack() const;
+    
 };
 
+template <typename T>
+StackUsingVector<T>::StackUsingVector(size_t sizeOfStack){
+    stackSize=sizeOfStack;
+    vectorOfData.reserve(stackSize);
+}
+
+template<typename T>
+bool StackUsingVector<T>::isEmpty() const{
+    if(dataCount==-1){
+        return true;
+    }
+    return false;
+}
+
+
+
+template <typename T>
+bool StackUsingVector<T>::isFull() const{
+    if(dataCount==stackSize-1){
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+bool StackUsingVector<T>::push(T data){
+    if(isFull()){
+        std::cout<<"Stack Is Full. We can't Push in More Elements "<<std::endl;
+        return false;
+    }
+    dataCount++;
+    vectorOfData.push_back(data);
+    return true;
+}
+
+
+template <typename T >
+T StackUsingVector<T>::pop(){
+    
+    if(isEmpty()){
+        throw std::underflow_error("Stack is Empty we can't Pop out Elements \n");
+    }
+    T data=vectorOfData.at(dataCount);
+    vectorOfData.pop_back();
+    dataCount--;
+    return data;
+}
+
+template <typename T >
+T StackUsingVector<T>::top() const{
+    if(isEmpty()){
+        throw std::underflow_error("Stack is Empty we can't see the Top Most Elements \n");
+    }
+    T data=vectorOfData.at(dataCount);
+    return data;
+}
+
+
+template<typename T>
+void StackUsingVector<T>::PrintStack() const{
+    if(isEmpty()){
+        std::cout<<"Stack is Empty !!"<<std::endl;
+    }
+    auto stackiterator = vectorOfData.cbegin();
+    std::cout<<"Elements in Stack are as Follows : "<<std::endl;
+    while(stackiterator!=vectorOfData.cend()){
+        std::cout<<*stackiterator<<std::endl;
+        stackiterator++;
+    }
+
+}
 
 }
 #endif /* StackUsingVector_hpp */
--- Stack/Stack/main.cpp
+++ Stack/Stack/main.cpp
@@ -8,6 +8,16 @@
 #include <iostream>
 #include "StackUsingVector.hpp"
 int main(int argc, const char * argv[]) {
-    mylib::StackUsingVector<int> integerStack;
+    mylib::StackUsingVector<int> integerStack{3};
+    std::cout<<std::boolalpha<<"Is Stack Empty ? "<<integerStack.isEmpty()<<std::endl;
+    std::cout<<"Is Stack Full ? "<<integerStack.isFull()<<std::endl;
+//    integerStack.top();
+//    integerStack.pop();
+    integerStack.push(1);
+    integerStack.push(2);
+    integerStack.push
+    (3);
+    integerStack.push(4);
+    integerStack.PrintStack();
     return 0;
 }
